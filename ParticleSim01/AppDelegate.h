0c80ce95cf3d54f6a470875233a8ffa56db37f99
Adriano <adrpapa@gmail.com>
2014-05-08 21:45:27 -0300

    first commit

---
+++ ParticleSim01/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParticleSim01
+//
+//  Created by Adriano Papa on 5/8/14.
+//  Copyright (c) 2014 Adriano Papa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
